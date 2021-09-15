d81975e4ff0ccba39616f1a0a12479a84b82fe8a
isdaft <ben.ashby2@gmail.com>
2021-09-15 06:29:22 -0400

    add new endpoint

--- functions/index.js
+++ functions/index.js
@@ -113,7 +113,101 @@ app.delete('/api/delete/:item_id', (req, res) => {
     }
     })();
 });
+//
+//
+//
+//
+// create
+app.post('/hplc_api/create', (req, res) => {
+    (async () => {
+        try {
+          await db.collection('shimadzu-hplc').add({
+            ERR_DATE_TIME: req.body.DATE_TIME,
+            HPLC_ID: req.body.HPLC_ID,
+            HPLC_STATE: req.body.STATE
+          }).then(ref => {
+              console.log('Added document with ID: ', ref.id);
+              console.log(req.body);
+              return null;
+            });
+          return res.status(200).send();
+        } catch (error) {
+          console.log(error);
+          return res.status(500).send(error);
+        }
+      })();
+  });
 
+// read item
+app.get('hplc_/api/read/:item_id', (req, res) => {
+    (async () => {
+        try {
+            const document = db.collection('shimadzu-hplc').doc(req.params.item_id);
+            let item = await document.get();
+            let response = item.data();
+            return res.status(200).send(response);
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send(error);
+        }
+        })();
+    });
+
+// read all
+app.get('hplc_api/read', (req, res) => {
+    (async () => {
+        try {
+            let query = db.collection('shimadzu-hplc');
+            let response = [];
+            await query.get().then(querySnapshot => {
+                let docs = querySnapshot.docs;
+                for (let doc of docs) {
+                    const selectedItem = {
+                        id: doc.id,
+                        item: doc.data()
+                    };
+                    response.push(selectedItem);
+                }
+                return response;
+            });
+            return res.status(200).send(response);
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send(error);
+        }
+        })();
+    });
+
+// update
+// app.patch('/hplc_api/update/:item_id', (req, res) => {
+// (async () => {
+//     try {
+//         const document = db.collection('shimadzu-hplc').doc(req.params.item_id);
+//         await document.update({
+//             ERR_DATE_TIME: req.body.ERR_DATE_TIME,
+//             HPLC_ID: req.body.title
+//         });
+//         return res.status(200).send();
+//     } catch (error) {
+//         console.log(error);
+//         return res.status(500).send(error);
+//     }
+//     })();
+// });
+
+// delete
+app.delete('hplc_api/delete/:item_id', (req, res) => {
+(async () => {
+    try {
+        const document = db.collection('shimadzu-hplc').doc(req.params.item_id);
+        await document.delete();
+        return res.status(200).send();
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send(error);
+    }
+    })();
+});
 
 
 
